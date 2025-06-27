c37535a6464c3e438ad12b585e83b22912f23218
Vaibhavk9675 <142034934+Vaibhavk9675@users.noreply.github.com>
2025-06-27 09:14:02 +0530

    flask

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:root@localhost/contact_page'
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(20), nullable = False)
+    ph_num = db.Column(db.String(12), nullable = False)
+    msg = db.Column(db.String(200), nullable = False)
+    date = db.Column(db.String(12))
 
 @app.route("/")
 def home():
@@ -10,8 +22,11 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods = ['POST', 'GET'])
 def contact():
+    if request.method=='POST':
+        '''add enrty to database'''
+
     return render_template("contact.html")
 
 @app.route("/projects")
