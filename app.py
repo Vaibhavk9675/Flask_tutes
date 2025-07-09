429c67a79bcf9e35df5bd0fcad8ee2759aeb7448
Vaibhavk9675 <142034934+Vaibhavk9675@users.noreply.github.com>
2025-07-09 08:16:26 +0530

    flask

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+# Use environment variables for DB connection
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    ph_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(12))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contact(name=name, email=email, ph_num=phone, msg=message,date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template("contact.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+# Run the app (Railway will inject PORT)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
